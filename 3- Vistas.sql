1eace291e57a5291ec628e5bdcd6fe14dbe268a1
matiasruzicska <matiasruzicska@gmail.com>
2026-03-16 22:40:48 -0300

    Update 3- Vistas.sql

--- 3- Vistas.sql
+++ 3- Vistas.sql
@@ -4,19 +4,18 @@
 CREATE VIEW vista_ventas_por_vendedor AS
 SELECT 
     v.id_vendedor,
-    e.nombre,
-    e.apellido,
+    CONCAT(e.nombre, ' ', e.apellido) AS vendedor,
     COUNT(ve.id_venta) AS cantidad_ventas
 FROM ventas ve
 JOIN vendedores v ON ve.id_vendedor = v.id_vendedor
 JOIN empleados e ON v.id_empleado = e.id_empleado
-GROUP BY v.id_vendedor;
+GROUP BY v.id_vendedor, e.nombre, e.apellido;
 
 -- VISTA PRODUCTO MAS VENDIDO
 
 CREATE VIEW vista_productos_mas_vendidos AS
 SELECT 
-    p.nombre,
+    p.nombre AS producto,
     SUM(dv.cantidad) AS total_vendido
 FROM detalle_venta dv
 JOIN productos p ON dv.id_producto = p.id_producto
@@ -29,7 +28,7 @@ CREATE VIEW vista_stock_por_local AS
 SELECT 
     l.nombre AS local,
     p.nombre AS producto,
-    i.stock
+    i.stock AS stock_disponible
 FROM inventario i
 JOIN locales l ON i.id_local = l.id_local
 JOIN productos p ON i.id_producto = p.id_producto;
@@ -38,18 +37,17 @@ JOIN productos p ON i.id_producto = p.id_producto;
 
 CREATE VIEW vista_ventas_por_cliente AS
 SELECT 
-    c.nombre,
-    c.apellido,
+    CONCAT(c.nombre, ' ', c.apellido) AS cliente,
     COUNT(v.id_venta) AS total_compras
 FROM ventas v
 JOIN clientes c ON v.id_cliente = c.id_cliente
-GROUP BY c.id_cliente;
+GROUP BY c.id_cliente, c.nombre, c.apellido;
 
 -- VISTA POR CATEGORIA
 
 CREATE VIEW vista_ventas_por_categoria AS
 SELECT 
-    cat.nombre_categoria,
+    cat.nombre_categoria AS categoria,
     SUM(dv.cantidad) AS total_vendido
 FROM detalle_venta dv
 JOIN productos p ON dv.id_producto = p.id_producto
